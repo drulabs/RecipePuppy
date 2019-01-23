b10db138485679259b570506088ba34a2caecccd
Kaushal <kaushal.devil009@gmail.com>
2019-01-23 07:58:05 +0530

    Added network module

---
+++ app/src/main/java/org/drulabs/recipepuppy/di/NetworkModule.java
@@ -0,0 +1,41 @@
+package org.drulabs.recipepuppy.di;
+
+import org.drulabs.data.entities.DataRecipe;
+import org.drulabs.data.repository.RemoteDataSource;
+import org.drulabs.network.api.RecipeService;
+import org.drulabs.network.mapper.NetworkDataMapper;
+import org.drulabs.network.mapper.NetworkMapper;
+import org.drulabs.network.source.NetworkDataSource;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.Provides;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+@Module
+abstract class NetworkModule {
+
+    @Binds
+    abstract RemoteDataSource bindNetworkDataSource(NetworkDataSource networkDataSource);
+
+    @Binds
+    abstract NetworkMapper<DataRecipe> bindsNetworkDataMapper(NetworkDataMapper mapper);
+
+    @Provides
+    RecipeService providesRecipeService(Retrofit retrofit) {
+        return retrofit.create(RecipeService.class);
+    }
+
+    @Provides
+    Retrofit providesRetrofitBuilder() {
+        return new Retrofit.Builder()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(RecipeService.BASE_URL)
+                .build();
+    }
+
+    // TODO add interceptors, logger, http client
+}
