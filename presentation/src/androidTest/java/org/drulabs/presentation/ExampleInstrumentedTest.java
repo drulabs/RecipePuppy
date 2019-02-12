f2998a02f2e357ddc757d1678aeee6955af10db3
Kaushal <kaushal.devil009@gmail.com>
2019-02-12 09:20:28 +0530

    updated useless files...duh.

--- presentation/src/androidTest/java/org/drulabs/presentation/ExampleInstrumentedTest.java
+++ presentation/src/androidTest/java/org/drulabs/presentation/ExampleInstrumentedTest.java
@@ -1,21 +1,22 @@
 package org.drulabs.presentation;
 
 import android.content.Context;
-import androidx.test.InstrumentationRegistry;
-import androidx.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import static org.junit.Assert.assertEquals;
 
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        Context appContext = InstrumentationRegistry.getInstrumentation().getContext();
 
         assertEquals("org.drulabs.presentation.test", appContext.getPackageName());
     }
